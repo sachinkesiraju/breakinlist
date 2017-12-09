de37dc9bc18834633b1800e71b9fe26e13b01e1a
Sachin Kesiraju <kesirajusachin@gmail.com>
2017-12-08 17:23:17 -0800

    in progress updated cb scraper
    cb changed their site which broke the scraper - this is my current progress getting it to work again

--- scripts/crunchbase.py
+++ scripts/crunchbase.py
@@ -7,7 +7,6 @@ import time
 import json
 import urllib
 import csv
-import pdb
 
 driver = webdriver.Safari()
 
@@ -28,26 +27,23 @@ def scrape_funding_details(company_url):
 		driver.close()
 	time.sleep(6 + randint(0, 3))
 
-	#WebDriverWait(driver, 15).until(jquery_complete,  "Timeout waiting for page to load")
 	driver.execute_script("var jq = document.createElement('script'); jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'; document.getElementsByTagName('head')[0].appendChild(jq);")
 	time.sleep(3)
 
-	#pdb.set_trace()
-
 	# Scrape funding history
-	#funding_table = driver.execute_script("return $('.grid-body').find('.grid-cell.flex.layout-row.layout-align-start-center')")
-	funding_data = driver.execute_script("var vals = []; $('.grid-body').find('.grid-cell.flex.layout-row.layout-align-start-center').each(function(index, val) { vals.push($.trim($(val).text())); }); return vals;")
+	funding_table = driver.execute_script("var vals = []; $('.component--grid-body').find('.component--grid-row').each(function(index, val) { vals.push($.trim($(val).text().replace(/\s{2,}/g,' '))); }); return vals;")
 	investors = []
-	for i in range(0, len(funding_data), 4):
-		investor_name = funding_data[i]
-		is_lead_investor = funding_data[i + 1]
+	for i in range(0, len(funding_table)):
+		funding_data = funding_table[i].split("-")
+		investor_name = funding_data[0]
+		is_lead_investor = funding_data[1]
 		if is_lead_investor.strip() == "-": is_lead_investor = "No"
-		funding_round = funding_data[i + 2]
+		funding_round = funding_data[2]
 		funding_round = funding_round.split('-')[0]
-		funding_partners = funding_data[i + 3]
+		funding_partners = funding_data[3]
 		funding_partners = funding_partners.replace('\n', '')
 		funding_partners = [partner.strip() for partner in funding_partners.split(',') if funding_partners.strip() != "-"]
-		print(investor_name + " " + is_lead_investor + " " + funding_round + " " + ','.join(funding_partners))
+		print("investor: "+investor_name + " " + is_lead_investor + " round: " + funding_round + " " + ','.join(funding_partners))
 		investor = {"investor_name": investor_name, "is_lead_investor": is_lead_investor, "funding_round": funding_round, "partners": funding_partners}
 		investors.append(investor)
 	return investors
@@ -66,8 +62,8 @@ try:
 		for row in reader: 
 			for (k, v) in row.items():
 				companies[k].append(v)
-		companies = companies['Company Name URL']
-		for company_url in companies[1920:]:
+		companies = companies['company_name_url']
+		for company_url in companies[2000:]:
 			print('\nReading company url: '+company_url)
 			funding_data = scrape_funding_details(company_url)
 			if not funding_data:
